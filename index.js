6cc60ca8aa9a608c477a221cc29e685e847a4952
Hunter Williams <hunter@huntergwilliams.com>
2021-03-11 08:03:50 +0700

    run via command line vs writing code

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
 const puppeteer = require('puppeteer');
 const helpers = require('./helpers');
 
+if (process.argv.length < 4) {
+  throw new Error('Expected 2 arguments: script.fscript "input"');
+}
+const fileToRun = process.argv[2];
+const input = process.argv[3];
+const shouldDebug = process.argv.length >= 5 ? process.argv[4] === "--debug" : false;
+
 (async () => {
 
-
-  const card = "Opt";
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  const sabai = helpers.fscriptify("sabai.fscript");
+  const scraper = helpers.fscriptify(fileToRun, shouldDebug);
 
-  const fizzy = helpers.fscriptify("fizzy.fscript");
-
-  const sabaiResults = await sabai(page, card);
-
-  const fizzyResults = await fizzy(page, card);
-
-  const results = sabaiResults.concat(fizzyResults);
+  const results = await scraper(page, input);
 
   await page.screenshot({
     path: "./screenshot.jpg",
     type: "jpeg",
     fullPage: true
-  });
+  });3
 
   await browser.close();
 
-  const filteredAndSorted = results.map(x => x.value).filter(x => x.name === card && x.quantity > 0).sort((a,b) => a.price - b.price);
+  //const filteredAndSorted = results.map(x => x.value).filter(x => x.name === card && x.quantity > 0).sort((a,b) => a.price - b.price);
+  // console.log("Cheapest: " + filteredAndSorted[0].price + "THB @ " + filteredAndSorted[0].link)
 
-  console.log("All Results --------------------------------");
-  console.log(filteredAndSorted);
-  console.log("Cheapest: " + filteredAndSorted[0].price + "THB @ " + filteredAndSorted[0].link)
-  console.log("done");
+  let hasError = false;
+  for (let i = 0; i < results.length; i += 1) {
+    const keys = Object.keys(results[i].error);
+    for (let j = 0; j < keys.length; j += 1) {
+      if (results[i].error[keys[j]] !== false) {
+        hasError = results[i].error[keys[j]];
+        break;
+      }
+    }
+  }
+
+  if (hasError !== false) {
+    console.log("Results have some error...");
+    console.log(hasError);
+    console.log("--------------------------------");
+    console.log("--------Results With Error------");
+    console.log(results);
+    console.log("----End of Results With Error---");
+  }
+
+  console.log("--------------Results-------------------");
+  console.log(results.map(x => x.value));
+  console.log("------------End of Results--------------");
 })();
