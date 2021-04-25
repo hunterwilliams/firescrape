dbf5699ecac0b07fadb2d5df67df6b7fa57be9b2
Hunter Williams <hunter@huntergwilliams.com>
2021-04-25 22:04:30 +0700

    handle args for running as command better

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const run = require('./runner').run;
 const DEBUG_FLAG = "--debug";
 const SERVER_FLAG = "--server";
 
-if (process.argv.length < 3) {
-  throw new Error('Expected at least 1 arguments: script.fscript "input"');
-}
-
 function containsFlag(flag) {
   for (let i = 0; i < process.argv.length; i++) {
     if (process.argv[i] === flag) {
@@ -19,15 +15,18 @@ const shouldDebug = containsFlag(DEBUG_FLAG);
 const runAsServer = containsFlag(SERVER_FLAG);
 const actualArgs = process.argv.filter(x => [DEBUG_FLAG, SERVER_FLAG].indexOf(x) === -1);
 
-const fileToRun = actualArgs[2];
-const input = actualArgs[3] || "";
-const output = actualArgs[4] || "";
-
 
 if (runAsServer) {
   console.log("Running as server");
 } else {
+  if (process.argv.length < 3) {
+    throw new Error('Expected at least 1 arguments: script.fscript "input"');
+  }
   console.log("Executing command");
+  const fileToRun = actualArgs[2];
+  const input = actualArgs[3] || "";
+  const output = actualArgs[4] || "";
+
   (async () => {
     run(fileToRun, input, output, shouldDebug)
   })();
