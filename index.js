4c6aa973b4b67211f79170d19d8ae60b665859d1
Hunter Williams <hunter@huntergwilliams.com>
2021-04-25 22:35:58 +0700

    initial express setup before queue

--- index.js
+++ index.js
@@ -18,6 +18,38 @@ const actualArgs = process.argv.filter(x => [DEBUG_FLAG, SERVER_FLAG].indexOf(x)
 
 if (runAsServer) {
   console.log("Running as server");
+  const express = require('express');
+  const fs = require('fs');
+  const PORT = process.env.PORT || 5000;
+  express()
+  .use(express.json())
+  .post('/test', async (req, res, next) => {
+    res.send(req.body);
+    next();
+  })
+  .post('/scrape', async (req, res, next) => {
+    const hrstart = process.hrtime()
+    const script = req.body.script;
+    const input = req.body.input || "";
+    const outputType = req.body.outputType || "";
+    const callbackUrl = req.body.callbackUrl;
+    if (!script || (outputType !== "" && outputType !== "json" && outputType !== "csv") || !callbackUrl) {
+      res.send("400");
+    } else {
+      if (!fs.existsSync(script)) {
+        res.send("404");
+      } else {
+        res.send("200");
+      }
+    }
+    const hrend = process.hrtime(hrstart);
+    const timing = `Execution time (hr): ${hrend[0]}s ${hrend[1] / 1000000}ms`;
+    console.log(timing);
+    next();
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
 } else {
   if (process.argv.length < 3) {
     throw new Error('Expected at least 1 arguments: script.fscript "input"');
